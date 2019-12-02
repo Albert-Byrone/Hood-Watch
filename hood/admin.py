3545e35cf281c779a7203bd7a9127b880afc1f30
Albert Byrone <albertbyrone1677@gmail.com>
2019-12-02 15:02:37 +0300

    register the different  models to the admin dashboard

--- hood/admin.py
+++ hood/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import NeighbourHood,Post,Profile,Business
 
-# Register your models here.
+admin.site.register(Post)
+admin.site.register(NeighbourHood)
+admin.site.register(Profile)
+admin.site.register(Business)
