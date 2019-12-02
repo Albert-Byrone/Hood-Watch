299217e11557c95d065661271c7ae0baf25e8591
Albert Byrone <albertbyrone1677@gmail.com>
2019-12-02 14:34:59 +0300

    create a function that allow users to signup

--- hood/views.py
+++ hood/views.py
@@ -15,5 +15,19 @@ def index(request):
     print(posts,"nnnnnnnnnnnnnn")
     return render(request,'main/index.html',{"posts":posts})
 
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = SignupForm()
+    return render(request, 'registration/registration_form.html', {'form': form})
+
 
 # Create your views here.
