d5c81018362a616278640baa24047ec1c46ef173
Albert Byrone <albertbyrone1677@gmail.com>
2019-12-02 13:58:55 +0300

    create a function that displays the quotes in the index page

--- hood/views.py
+++ hood/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
+from django.contrib.auth import login,authenticate
+from django.contrib.auth.decorators import login_required
+from .models import Profile,NeighbourHood,Post,Business
+from django.http import HttpResponseRedirect
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from .forms import PostForm,UpdateProfileForm,NeighbourHoodForm,BusinessForm,SignupForm
+
+
+@login_required(login_url='/accounts/login')
+def index(request):
+    posts = Post.objects.all()
+    posts = posts[::-1]
+    print(posts,"nnnnnnnnnnnnnn")
+    return render(request,'main/index.html',{"posts":posts})
+
 
 # Create your views here.
